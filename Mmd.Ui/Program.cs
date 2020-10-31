c9efc76354dc1fed09a73a9cff946f200a950505
Shraddhesh Singh <sids@protonmail.com>
2020-10-31 07:32:18 +0530

    small change

--- Mmd.Ui/Program.cs
+++ Mmd.Ui/Program.cs
@@ -17,8 +17,11 @@ namespace mmd_plus
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        }
+            
     }
 }
